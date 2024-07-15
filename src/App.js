beb6d8a0ed0a7ca9e48ad6d4bc55dcd5c292d80f
unknown <krishnabhargavvakkalagadda@gmail.com>
2024-07-15 21:07:28 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,26 @@ function App() {
               <Customcard key={eachItem.id} item={eachItem}></Customcard>
             );
           })):
-              <h6>Content not found</h6>
+              <h6>Content not found or List is empty</h6>
+        }
+      </div>
+
+      <div>
+        {
+          List.map((eachItem) => {
+            var c="";
+            var c = eachItem.id%2==0?"d1":"d2"; 
+            var status = eachItem.id%2==0?"even":"odd"; 
+
+            return(
+
+              <>
+              <div className={c} style={{width:'80px' , height:'50px', display:"inline-block" , margin:"10px"}}>
+              </div>
+              <div style={{display:"inline-block"}}>This is {status}</div><br/>
+              </>
+            );
+          })
         }
       </div>
     </div>
--- src/Components/card/card.js
+++ src/Components/card/card.js
@@ -3,21 +3,21 @@ import Card from 'react-bootstrap/Card';
 import CustomList from '../Olist/olist';
 import "./card.css"
 
-var count = 2;
+// var count = 2;
 function Customcard(prop) {
 
     const {item} = prop ;
-    var c = count%2==0?"red":"blue";
+    var c = item.id%2==0?"red":"blue";
     console.log(c);
-    count++;
+    // count+=1;
   return (
     <Card style={{ width: '30%' , margin: '10px'}}>
       <Card.Img variant="top" src={item.image} />
       <Card.Body>
-        <Card.Title>{item.name}</Card.Title>
-        <h6 style={{color:'red', margin:'10px'}}>Ingredients :</h6>
+        <Card.Title className={c}>{item.name}</Card.Title>
+        <h6 style={{color:'purple', margin:'10px'}}>Ingredients :</h6>
         <CustomList iterable={item.ingredients}></CustomList>
-        <h6 style={{color:'red', margin:'10px'}}>Instructions :</h6>
+        <h6 style={{color:'purple', margin:'10px'}}>Instructions :</h6>
         <CustomList iterable={item.instructions}></CustomList>
         {/* <Button variant="primary">Restaurante</Button> */}
       </Card.Body>
