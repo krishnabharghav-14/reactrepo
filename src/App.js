b29ac91dd10ade613c28923123053ed40bbb3d60
unknown <krishnabhargavvakkalagadda@gmail.com>
2024-07-24 17:58:03 +0530

    added task 2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Customcard from './components/card/card';
 import "bootstrap/dist/css/bootstrap.min.css";  
+import AddSquare from './components/addSquare/addSquare';
 
 function App() {
   return (
     <div className="App">
-      <Customcard/>
+      <AddSquare/>
+      {/* <Customcard/> */}
     </div>
   );
 }
