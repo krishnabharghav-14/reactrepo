b39302eb48a175cd0f9567a44659850273bbcc58
unknown <krishnabhargavvakkalagadda@gmail.com>
2024-07-30 11:03:47 +0530

    added all source files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import Uncontrolled from './components/form/form';
+import Uncontrolled from './components/form/uncontrolled/form';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import RegistrationForm from './components/form/registrationForm';
-import SamsungForm from './components/form/samsungForm';
+import RegistrationForm from './components/form/uncontrolled/registrationForm1';
+import SamsungForm from './components/form/uncontrolled/samsungForm';
+import PracticeControlledForm from './components/form/controlled/controlledform';
+import ControlledRegistrationForm from './components/form/controlled/registrationFrom2';
+import ControlledLoginForm from './components/form/controlled/loginForm';
 
 function App() {
   return (
     <div className="App">
       {/* <Uncontrolled/> */}
       {/* <RegistrationForm/> */}
-      <SamsungForm/>
+      {/* <SamsungForm/> */}
+      {/* <PracticeControlledForm/> */}
+      <ControlledRegistrationForm/>
+      {/* <ControlledLoginForm/> */}
     </div>
   );
 }
